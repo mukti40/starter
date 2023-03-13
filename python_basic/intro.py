008ba73b85ff47cf0ea3990fc8e582f0df8109af
Mukti Mukesh Das <71608616+mukti40@users.noreply.github.com>
2023-03-13 16:36:15 +0530

    Update intro.py

--- python_basic/intro.py
+++ python_basic/intro.py
@@ -1,1 +1,22 @@
 print("Hello World")
+
+name=input()
+age=int(input("enter your age:"))
+
+## arithmetic Operators ( + , - , * , / , // , % )
+
+## Logical Operator ( == , >= , <= , > , < , != )
+
+## Boolean Operator ( and , or , not )
+
+len()
+
+## Typecasting-
+int()
+float()
+bin()
+str()
+oct()
+hex()
+
+
